f9b26bfcd3c521b792b22d836f127727dea1dde2
Zongyang Li <zyl220@nyu.edu>
2017-05-03 18:25:22 -0400

    Mobility Finish

--- Mobility.R
+++ Mobility.R
@@ -19,9 +19,8 @@ library(mlogit)
 family_head_all_restrict_final_2 <- read.dta13("family_head_all_restrict_final_2.dta")
 
 
-
 ################################################
-#
+# Mobility MLS
 ################################################
 
 mobility_mls <- family_head_all_restrict_final_2 %>% 
@@ -29,11 +28,61 @@ mobility_mls <- family_head_all_restrict_final_2 %>%
 	group_by(fid10) %>% 
 	spread(year,category_mls) 
 
+names(mobility_mls) <- c('fid10','yr2010','yr2012','yr2014')
+
+mobility_mls_1012 <- mobility_mls%>% 
+	group_by(yr2010) %>%
+	dplyr::summarise(Stable = sum(ifelse(yr2012==0,1,0),na.rm = T),
+			Poor = sum(ifelse(yr2012==1,1,0),na.rm = T),
+			Transient_Stable = sum(ifelse(yr2012==2,1,0),na.rm = T),
+			Transient_Poor = sum(ifelse(yr2012==3,1,0),na.rm = T)
+		) %>%ungroup()
+
 mobility_mls_1214 <- mobility_mls%>% 
-	group_by()
+	group_by(yr2012) %>%
+	dplyr::summarise(Stable = sum(ifelse(yr2014==0,1,0),na.rm = T),
+			Poor = sum(ifelse(yr2014==1,1,0),na.rm = T),
+			Transient_Stable = sum(ifelse(yr2014==2,1,0),na.rm = T),
+			Transient_Poor = sum(ifelse(yr2014==3,1,0),na.rm = T)
+		) %>%ungroup()
 
 
+################################################
+# Mobility WB
+################################################
 
+mobility_wb <- family_head_all_restrict_final_2 %>% 
+	select(fid10,category_wb,year) %>% 
+	group_by(fid10) %>% 
+	spread(year,category_wb) 
+
+names(mobility_wb) <- c('fid10','yr2010','yr2012','yr2014')
+
+mobility_wb_1012 <- mobility_wb%>% 
+	group_by(yr2010) %>%
+	dplyr::summarise(Stable = sum(ifelse(yr2012==0,1,0),na.rm = T),
+			Poor = sum(ifelse(yr2012==1,1,0),na.rm = T),
+			Transient_Stable = sum(ifelse(yr2012==2,1,0),na.rm = T),
+			Transient_Poor = sum(ifelse(yr2012==3,1,0),na.rm = T)
+		) %>%ungroup()
+
+mobility_wb_1214 <- mobility_wb%>% 
+	group_by(yr2012) %>%
+	dplyr::summarise(Stable = sum(ifelse(yr2014==0,1,0),na.rm = T),
+			Poor = sum(ifelse(yr2014==1,1,0),na.rm = T),
+			Transient_Stable = sum(ifelse(yr2014==2,1,0),na.rm = T),
+			Transient_Poor = sum(ifelse(yr2014==3,1,0),na.rm = T)
+		) %>%ungroup()
+
+
+################################################
+# EXPORT
+################################################
+
+write.csv(mobility_mls_1012,file='mobility_mls_1012.csv')
+write.csv(mobility_mls_1214,file='mobility_mls_1214.csv')
+write.csv(mobility_wb_1012,file='mobility_wb_1012.csv')
+write.csv(mobility_wb_1214,file='mobility_wb_1214.csv')
 
 data("Train", package = "mlogit")
 Tr <- mlogit.data(Train, shape = "wide", varying = 4:11, choice = "choice",
